2647ff3bd5b8630450413c04b17d63bb5b3325e0
vishal <vishalgiri8507012571@gmail.com>
2023-06-14 09:46:36 +0530

    initial commit from v1.0.1

---
+++ src/components/Header.js
@@ -0,0 +1,25 @@
+export const Header = () => {
+  return (
+    <header style={styles.header}>
+      <h1 style={styles.headerText}>VG's Drive</h1>
+      <p style={styles.headerSubText}>
+        A File Manager created by vishalgiri8888
+      </p>
+    </header>
+  );
+};
+
+const styles = {
+  header: {
+    backgroundColor: "#333",
+    color: "#fff",
+    textAlign: "center",
+    padding: "10px",
+  },
+  headerText: {
+    fontSize: "30px",
+  },
+  headerSubText: {
+    fontSize: "15px",
+  },
+};
